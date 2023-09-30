fede71c22167213560eef232a04359fdcc61a417
ksuviper <ksuviper@gmail.com>
2023-09-30 00:36:16 -0500

    Remove artists.csv dataset Imported project 1 code to enhance

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 # Adam Staggenborg
 # Mini Project 2
 
-
-# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
-# (5/5 points) Proper import of packages used.
+# x(5/5 points) Initial comments with your name, class and project at the top of your .py file.
+# x(5/5 points) Proper import of packages used.
 # (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
 #   Think of some question you would like to solve such as:
 #   "How many homes in the US have access to 100Mbps Internet or more?"
@@ -16,3 +15,98 @@
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # (10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations.
+
+
+from pathlib import Path
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pprint
+
+# data = pd.read_csv('', index_col='')
+# print(data.head())
+
+# Apple = APPL
+# Microsoft = MSFT
+# Google = GOOG
+# GameStop = GME
+# Facebook = META
+
+def getClosing(ticker):
+    stock = yf.Ticker(ticker)
+    # get historical market data
+    hist = stock.history(period="10d")
+
+    closingList = []
+
+    # loop through closing prices and add to list
+    for price in hist['Close']:
+        closingList.append(round(price, 2))
+
+    return closingList
+
+def printGraphs(stock):
+    # get closing prices for stock
+    closingPrices = np.array(getClosing(stock))
+    days = list(range(1, len(closingPrices) + 1))
+
+    # get min and max for the y axis
+    prices = getClosing(stock)
+    prices.sort()
+    lowPrice = prices[0]
+    highPrice = prices[-1]
+
+    # plot stock closing prices
+    plt.plot(days, closingPrices)
+
+    # set axis min and max values
+    plt.axis([1, 10, lowPrice - 2, highPrice + 2])
+
+    # set plot labels
+    plt.title("Stock Closing Prices - " + stock)
+    plt.xlabel("Days")
+    plt.ylabel("Closing Price")
+
+    # save the graph
+    plt.savefig("charts/" + stock + ".png")
+
+    # show the graph
+    plt.show()
+
+def getStocks():
+    stocks = []
+
+    print("Please enter 5 stocks to graph:")
+
+    for i in range(1, 6):
+
+        # Get stocks from user
+        while True:
+            print("Enter stock ticker number " + str(i))
+            ticker = input("> ")
+            # check validity of enter stock and add to list
+            print(ticker)
+            try:
+                print("Checking Ticker...")
+                stock = yf.Ticker(ticker)
+                stock.info
+                stocks.append(ticker)
+                print("Valid Ticker.")
+                break
+            except:
+                print("That is not a valid stock. Please enter another.")
+
+    return stocks
+
+
+# start program
+# create empty directory to save charts
+try:
+    Path("charts").mkdir()
+except FileExistsError:
+    pass
+
+for stock in getStocks():
+    getClosing(stock)
+    printGraphs(stock)
